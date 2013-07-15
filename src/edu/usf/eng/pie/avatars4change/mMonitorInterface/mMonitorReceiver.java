d5436cea7c98b94cc74404e8eeebcc8bbd5d27e5
7yl4r <murray.tylar@gmail.com>
2013-07-15 16:34:00 -0400

    mMonitor broadcasts now being received

--- src/edu/usf/eng/pie/avatars4change/mMonitorInterface/mMonitorReceiver.java
+++ src/edu/usf/eng/pie/avatars4change/mMonitorInterface/mMonitorReceiver.java
@@ -8,67 +8,64 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 //import edu.usf.eng.pie.avatars4change.R;	//import for the R object???
 
 //for receiving the incoming data
 public class mMonitorReceiver extends BroadcastReceiver {
-
+	private static final String TAG = "Avatars4Change mMonitorReceiver";
+	
+//	public static final String DAILY_STATE_CHANGE_INTENT = "com.MILES.MonitoringApp.DailyStateChange";
+	public static final String SEC_LEVEL_BROADCAST_INTENT = "com.MILES.MonitoringApp.secLevelData"; 
+	public static final String SEC_LEVEL_BROADCAST_NAME   = SEC_LEVEL_BROADCAST_INTENT;
+	
 	@Override
 	public void onReceive(Context context, Intent intent) {	
-		
 		//verify intent is coming from expected application (everything should be moved into this if once working properly)
-		if(intent.getAction().equals("DAILY_STATE_CHANGE_INTENT")){		//perhaps this is not the proper action to listen for?
-			Toast.makeText(context, "DAILY_STATE_CHANGE_INTENT recieved\n",
-					Toast.LENGTH_LONG).show();
+		if (intent.getAction().equals(SEC_LEVEL_BROADCAST_INTENT)){
+			//Toast.makeText(context, "SEC_LEVEL_BROADCAST_INTENT "+SEC_LEVEL_BROADCAST_INTENT+" broadcast received\n",
+			//		Toast.LENGTH_SHORT).show();
+			Log.d(TAG,"SEC_LEVEL_BROADCAST_INTENT "+SEC_LEVEL_BROADCAST_INTENT+" broadcast received");
+			
+			//read in broadcast value
+			Bundle extras = intent.getExtras();
+			double value = extras.getDouble(SEC_LEVEL_BROADCAST_NAME);
+			Log.d(TAG,"received broadcast value = "+Double.toString(value));
+					
+			//send notification to show output:
+				//get ref to NotificationManager
+			String ns = Context.NOTIFICATION_SERVICE;
+			NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(ns);
+				//initiate the notification
+			int icon = R.drawable.thumb; //(notification icon)
+
+			CharSequence tickerText = "Avatar receiving data from mMonitor...";
+			long when = System.currentTimeMillis();
+			
+			Notification notification = new Notification(icon, tickerText, when);
+				//Define notificaiton's message and PendingIntent:
+			CharSequence contentTitle = "mDisplay Activity Notifier";
+			CharSequence contentText = "mMonitor BroadCast: " + Double.toString(value);
+			PendingIntent contentIntent = PendingIntent.getActivity(context,0, intent, 0);
+			notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+				//Pass notification to manager
+			final int HELLO_ID = 1;
+			mNotificationManager.notify(HELLO_ID, notification);
+			
+			/*
+			//To add vibration, you must include vibrate access permission
+			//vibrate
+			Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+			vibrator.vibrate(2000);
+			*/
+			
 		} else {
 			Toast.makeText(context, "application intent not verified\n",
-					Toast.LENGTH_LONG).show();
+					Toast.LENGTH_SHORT).show();
 		}
 		
-		//read in broadcast value
-		Bundle extras = intent.getExtras();
-		int value = 0; 
-		String valString = "ERR: valString Empty";
-		//string to int
-		try {
-			valString = extras.getString("DailyStateChangeValue");		//this does not work; string returned is "null", parse error
-		    //valString = extras.getString("DAILY_STATE_CHANGE_INTENT");	//this does not work either, same as above
-			value = Integer.parseInt(valString);
-		} catch(NumberFormatException nfe) {	
-		   System.out.println("Could not parse " + nfe);
-		}
 		
-		//value = extras.getInt("DailyStateChangeValue");		//this does not work, value returned is '0'
-		//value = extras.getInt("DAILY_STATE_CHANGE_INTENT");		//this does not work either, same as above
-		
-				
-		//send notification to show output:
-			//get ref to NotificationManager
-		String ns = Context.NOTIFICATION_SERVICE;
-		NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(ns);
-			//initiate the notification
-		int icon = R.drawable.thumb; //(notification icon)
-
-		CharSequence tickerText = "mMonitor's TickerText";
-		long when = System.currentTimeMillis();
-		
-		Notification notification = new Notification(icon, tickerText, when);
-			//Define notificaiton's message and PendingIntent:
-		CharSequence contentTitle = "mDisplay Activity Notifier";
-		CharSequence contentText = "mMonitor BroadCast: " + valString + "=" + value;
-		PendingIntent contentIntent = PendingIntent.getActivity(context,0, intent, 0);
-		notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
-			//Pass notification to manager
-		final int HELLO_ID = 1;
-		mNotificationManager.notify(HELLO_ID, notification);
-		
-		/*
-		//To add vibration, you must include vibrate access permission
-		//vibrate
-		Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-		vibrator.vibrate(2000);
-		*/
 		
 	}
 }
